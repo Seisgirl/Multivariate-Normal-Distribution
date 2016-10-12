c3bba537407cb5b9a84681cdd5ffdb92e5756ca7
tphysics <toshi.tanu08@gmail.com>
2016-10-12 09:26:54 +0530

    Add files via upload

---
+++ Q_1.py
@@ -0,0 +1,36 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+#PART_ONE
+x1 = np.linspace(-100.0, 100.0, 5000.0)
+y1 = np.linspace(-100.0, 100.0, 5000.0)
+X, Y = np.meshgrid(x1, y1) #seting up a symmertrical meshgrid
+Z = (6*X**2 + 9*Y**2 + 4*X*Y)/(1000*(2**0.5)*math.pi)
+plt.figure()
+plot = plt.contourf(X, Y, Z)
+plt.colorbar()
+plt.title('Contour Map of Probability Distribution Function of x and y')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+#PART_TWO
+mean = [0, 0] #given means of the two vbls are 0
+covMat = [[9, -2], [-2, 6]] #covariance matrix
+x, y = np.random.multivariate_normal(mean, covMat, 50000).T #Part(b)generating 500 pairs of random normally distributed variables
+Normal_Multivariate, xt, yt = np.histogram2d(x, y, bins=100, normed=True) #Part (c) plotting a heatmap (2d histogram) with number of bins for both dimentions = 100
+extent = [xt[0], xt[-1], yt[0], yt[-1]] #setting the bin edges along each direction
+plt.imshow(Normal_Multivariate, extent=extent, cmap='spectral') #printing the plot
+plt.title("Heatmap/Histogram of Normal Bivariate Distribution")
+plt.ylabel("y")
+plt.xlabel("x")
+plt.colorbar() #adding colorbar to chart values corresponding to colors
+plt.show()
+#PART_THREE
+z = (6*x**2 + 9*y**2 + 4*x*y)/50 #introducing z
+plt.clf() #clearing the plot, resettin axes scales
+plt.hist(z, 100, normed=1, facecolor='cyan')  # Plotting the histogram. Number of bins = 100
+plt.title("Histogram for Probability Density of z")
+plt.ylabel("Probability Density")
+plt.xlabel("z")
+plt.legend()
+plt.show()
