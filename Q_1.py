139ee91736287eb66a3f58686115135734845622
tphysics <toshi.tanu08@gmail.com>
2016-10-12 17:39:20 +0530

    Update Q_1.py

--- Q_1.py
+++ Q_1.py
@@ -2,12 +2,12 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 #PART_ONE
-x1 = np.linspace(-100.0, 100.0, 5000.0)
-y1 = np.linspace(-100.0, 100.0, 5000.0)
+x1 = np.linspace(-10.0, 10.0, 5000.0)
+y1 = np.linspace(-10.0, 10.0, 5000.0)
 X, Y = np.meshgrid(x1, y1)                                                #seting up a symmertrical meshgrid
-N = (6*X**2 + 9*Y**2 + 4*X*Y)/(1000*(2**0.5)*math.pi)                     #expression for pdf
+N = (math.e**(-(6*X**2 + 9*Y**2 + 4*X*Y)/100))/(10*math.sqrt(2)*math.pi)                    #expression for pdf
 plt.figure()
-plot = plt.contourf(X, Y, N)
+plot = plt.contour(X, Y, N)
 plt.colorbar()
 plt.title('Contour Map of Probability Distribution Function of x and y')
 plt.xlabel('x')
